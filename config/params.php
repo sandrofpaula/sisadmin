25739eabd4271f79bf394fa5d622c5d4a9678c67
SANDRO FONSECA PAULA <sandro.fonseca@semed.manaus.am.gov.br>
2021-06-10 21:31:33 -0400

    correção inesperada

--- config/params.php
+++ config/params.php
@@ -4,5 +4,4 @@ return [
     'adminEmail' => 'admin@example.com',
     'senderEmail' => 'noreply@example.com',
     'senderName' => 'Example.com mailer',
-    'icon-framework' => \kartik\icons\Icon::FAS,  // Font Awesome Icon framework
 ];
